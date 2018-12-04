5df2e34ffca5c5f03b953c7037880adb0ace8bf2
Italo Oliveira <italowco@gmail.com>
2018-12-04 15:21:33 -0300

    testando metodos do panda

--- 01_Preprocessing/diabetes_csv.py
+++ 01_Preprocessing/diabetes_csv.py
@@ -24,6 +24,13 @@ feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
 X = data[feature_cols]
 y = data.Outcome
 
+
+#round(float(X.DiabetesPedigreeFunction), 3)
+
+X['DiabetesPedigreeFunction'] = pd.to_numeric(X.loc['DiabetesPedigreeFunction'], downcast='float')
+
+#print(X['DiabetesPedigreeFunction'])
+
 # Ciando o modelo preditivo para a base trabalhada
 print(' - Criando modelo preditivo')
 neigh = KNeighborsClassifier(n_neighbors=3)
@@ -38,15 +45,14 @@ y_pred = neigh.predict(data_app)
 URL = "https://aydanomachado.com/mlclass/01_Preprocessing.php"
 
 #TODO Substituir pela sua chave aqui
-DEV_KEY = "COLOCAR_SUA_KEY_AQUI"
+DEV_KEY = "IWCO"
 
 # json para ser enviado para o servidor
-data = {'dev_key':DEV_KEY,
-        'predictions':pd.Series(y_pred).to_json(orient='values')}
+# data = {'dev_key':DEV_KEY, 'predictions':pd.Series(y_pred).to_json(orient='values')}
 
 # Enviando requisição e salvando o objeto resposta
-r = requests.post(url = URL, data = data)
+# r = requests.post(url = URL, data = data)
 
 # Extraindo e imprimindo o texto da resposta
-pastebin_url = r.text
-print(" - Resposta do servidor:\n", r.text, "\n")
+# pastebin_url = r.text
+# print(" - Resposta do servidor:\n", r.text, "\n")
