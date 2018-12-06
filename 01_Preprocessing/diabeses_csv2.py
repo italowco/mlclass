f6e2695985093a277d2d07e25425061234eb6bb9
Italo Oliveira <italowco@gmail.com>
2018-12-06 16:56:34 -0300

    77%

---
+++ 01_Preprocessing/diabeses_csv2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Atividade para trabalhar o pré-processamento dos dados.
+
+Criação de modelo preditivo para diabetes e envio para verificação de peformance
+no servidor.
+
+@author: Aydano Machado <aydano.machado@gmail.com>
+"""
+
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import requests
+
+print('\n - Lendo o arquivo com o dataset sobre diabetes')
+df = pd.read_csv('diabetes_dataset.csv')
+
+# Criando X and y par ao algorítmo de aprendizagem de máquina.\
+print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
+# Caso queira modificar as colunas consideradas basta algera o array a seguir.
+feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'BMI', 'Age']
+
+#X = data[feature_cols].dropna()
+
+dataNew = df[feature_cols].fillna(df[feature_cols].mean())
+
+X = dataNew
+y = df.Outcome
+
+# Ciando o modelo preditivo para a base trabalhada
+print(' - Criando modelo preditivo')
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X, y)
+
+#realizando previsões com o arquivo de
+print(' - Aplicando modelo e enviando para o servidor')
+data_app = pd.read_csv('diabetes_app.csv')
+y_pred = neigh.predict(data_app[feature_cols])
+print(y_pred)
+# Enviando previsões realizadas com o modelo para o servidor
+URL = "https://aydanomachado.com/mlclass/01_Preprocessing.php"
+
+#TODO Substituir pela sua chave aqui
+DEV_KEY = "IWCO"
+
+# json para ser enviado para o servidor
+data = {'dev_key':DEV_KEY,'predictions':pd.Series(y_pred).to_json(orient='values')}
+
+# Enviando requisição e salvando o objeto resposta
+r = requests.post(url = URL, data = data)
+
+# Extraindo e imprimindo o texto da resposta
+pastebin_url = r.text
+print(" - Resposta do servidor:\n", r.text, "\n")
